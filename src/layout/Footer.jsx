a3769cfdfde86729bcf9bc1b9ea8dc563aee5b7a
Олександр Бавдик <sbavdyk@MacBook-Pro-Oleksandr.local>
2022-10-11 16:18:45 +0300

    project complete

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,21 @@
+
+
+const Footer = () => {
+    return (
+        <div>
+            <footer className="page-footer blue lighten-2">
+                <div className="footer-copyright">
+                    <div className="container">
+                        © 2022 Copyright Text
+                        <a className="grey-text text-lighten-4 right" href="#!">Repo</a>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    )
+}
+
+
+
+
+export default Footer;
---
+++ src/layout/Header.jsx
@@ -0,0 +1,17 @@
+
+
+const Header = () => {
+    return (
+        <nav className="blue">
+            <div className="nav-wrapper">
+                <a href="!#" className="brand-logo">React Movies</a>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <li><a href="!#">JavaScript</a></li>
+                </ul>
+            </div>
+        </nav>
+    )
+
+}
+
+export default Header;
